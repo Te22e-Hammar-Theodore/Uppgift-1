3ded14a6fbb1244c877a251ea68b63134018882c
Theodore Hammar <theodore.hammar@elev.ga.ntig.se>
2023-09-06 10:12:36 +0200

    tal1 & tal2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int tal1 = 2;
+        int tal2 = 30;
+
+        int summa_addition = tal1+tal2;
+        int summa_subraktion = tal1-tal2;
     }
 }
